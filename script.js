a5837a32be505c794b9272c6726aecdbe1007bf0
Jon Blacksmith <jon@jix.ca>
2021-10-20 18:49:18 -0400

    Added core functionality

---
+++ script.js
@@ -0,0 +1,67 @@
+function computerSelection() {
+  let options = ['rock', 'paper', 'scissors'];
+  return (options[Math.floor(Math.random() * options.length)]);
+}
+
+function playerSelection() {
+  let playerInput = prompt("Type Rock, Paper, or Scissors: ");
+  let trimInput = playerInput.trim();
+  let lowerInput = trimInput.toLowerCase();
+
+  while (!['rock', 'paper', 'scissors'].includes(lowerInput)) {
+    playerInput = prompt("Invalid input. Please choose Rock, Paper, or Scissors: ");
+    trimInput = playerInput.trim();
+    lowerInput = trimInput.toLowerCase();
+  }
+  return lowerInput;
+}
+
+function playRound() {
+
+  let user = playerSelection();
+  let cpu = computerSelection();
+
+  if (user === cpu) {
+    return 'tie';
+  }
+  else if (user === 'rock' && cpu === 'scissors' || user === 'paper' && cpu === 'rock' || user === 'scissors' && cpu === 'paper') {
+    return 'win';
+  }
+  else {
+    return 'lose';
+  }
+}
+
+
+function game() {
+  let cpuScore = 0;
+  let userScore = 0;
+  let roundCount = 0;
+  let roundResult = 0;
+
+  while (roundCount <= 4) {
+    roundResult = playRound();
+    if (roundResult == 'win') {
+      userScore++;
+    }
+    else if (roundResult == 'lose') {
+      cpuScore++;
+    }
+    roundCount++;
+  }
+
+  if (userScore > cpuScore) {
+    console.log("You win!");
+  }
+  else if (cpuScore > userScore) {
+    console.log("You lose, try again!");
+  }
+  else {
+    console.log("Tied!");
+  }
+
+  console.log(`Your score: ${userScore}`);
+  console.log(`Computer score: ${cpuScore}`);
+}
+
+game();
